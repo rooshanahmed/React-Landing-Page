d0bd852ca674113ab47211723430908eb17ceb88
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-09-21 02:19:49 +0500

    App Updated

--- src/components/PlaceToVisit.js
+++ src/components/PlaceToVisit.js
@@ -1,27 +1,27 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core'
-import ImageCard from './imageCard'
-import places from '../static/places'
-import useWindowPosition from '../hook/useWindowPosition'
+import React from "react";
+import { makeStyles } from "@material-ui/core";
+import ImageCard from "./imageCard";
+import places from "../static/places";
+import useWindowPosition from "../hook/useWindowPosition";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        minHeight: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-}))
+  root: {
+    minHeight: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+}));
 
 export default function PlaceToVisit() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    const checked = useWindowPosition('header');
+  const checked = useWindowPosition("header");
 
-    return(
-        <div className={classes.root}>
-            <ImageCard place={places[1]} checked={checked} />
-            <ImageCard place={places[0]} checked={checked} />
-        </div>
-    )
+  return (
+    <div className={classes.root}>
+      <ImageCard place={places[1]} checked={checked} />
+      <ImageCard place={places[0]} checked={checked} />
+    </div>
+  );
 }
